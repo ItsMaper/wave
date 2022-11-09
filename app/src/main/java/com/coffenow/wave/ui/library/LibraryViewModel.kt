67f828f0213937e143da72a8c0b3e58d052c29ae
JDavidCode <davidanayaacosta@gmail.com>
2022-11-08 21:17:31 -0500

    0.1.1

--- app/src/main/java/com/coffenow/wave/ui/library/LibraryViewModel.kt
+++ app/src/main/java/com/coffenow/wave/ui/library/LibraryViewModel.kt
@@ -1,13 +1,13 @@
-package com.coffenow.wave.ui.notifications
+package com.coffenow.wave.ui.library
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class LibraryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is library Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/coffenow/wave/ui/user/UserFragment.kt
+++ app/src/main/java/com/coffenow/wave/ui/user/UserFragment.kt
@@ -1,4 +1,4 @@
-package com.coffenow.wave.ui.dashboard
+package com.coffenow.wave.ui.user
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.coffenow.wave.databinding.FragmentDashboardBinding
+import com.coffenow.wave.databinding.FragmentUserBinding
 
-class DashboardFragment : Fragment() {
+class UserFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentUserBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val userViewModel =
+            ViewModelProvider(this).get(UserViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentUserBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        userViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
